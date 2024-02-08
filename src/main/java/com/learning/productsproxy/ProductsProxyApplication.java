796afb5a57783cdbb3439b2289b39c99ee45fb5f
Parichay Gupta <parichay.gupta@aa.com>
2024-02-08 21:23:09 +0530

    Create Spring Boot Project

---
+++ src/main/java/com/learning/productsproxy/ProductsProxyApplication.java
@@ -0,0 +1,12 @@
+package com.learning.productsproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsProxyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/productsproxy/ProductsProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.productsproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
